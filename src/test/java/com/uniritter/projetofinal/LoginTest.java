1783bee12ac492c65b67daa48add5c13c2333690
DieguinhoHR <diego_henriquerodrigues@hotmail.com>
2016-04-19 02:19:23 -0300

    Created test for login

--- src/test/java/com/uniritter/projetofinal/LoginTest.java
+++ src/test/java/com/uniritter/projetofinal/LoginTest.java
@@ -1,8 +1,10 @@
 package com.uniritter.projetofinal;
 
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
